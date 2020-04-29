eae5a9e030b9b37659cc34dae15b8bb7dfc52b10
jaywei1234 <jaywei829@gmail.com>
2020-04-29 17:44:16 +0800

    final version

--- api/controllers/CoachController.js
+++ api/controllers/CoachController.js
@@ -41,23 +41,36 @@ module.exports = {
 
     if (req.method == 'POST') {
       var coach = await Coach.create(req.session.data).fetch();
-
-
+    //  coach.ChiName=req.session.ChiName;
+    //  coach.EngName=req.session.EngName;
+     coach.FormNum=req.session.Email;
+     req.session.FormSub="yes";
+     User.FormSub="yes";
+     coach.FormSubmit="yes";
+     
+     
 
       //  await Coach.create(req.body.Coach);
 
       //  req.session.data = {};  //clear data of session
 
-      // console.log(coach.Email);
-      // var models = await User.find();
-      // var html = await sails.renderView('membership/Email3', { coach: coach, layout: false });
-      // await sails.helpers.sendSingleEmail({
-      // to: coach.Email,
-      // from: sails.config.custom.mailgunFrom,
-      // subject: '已收到閣下的申請表',
-      // html: html
-      // });
+      console.log(coach.Email);
+      var models = await User.find();
+      var html = await sails.renderView('membership/Email3', { coach: coach, layout: false });
+      await sails.helpers.sendSingleEmail({
+      to: coach.Email,
+      from: sails.config.custom.mailgunFrom,
+      subject: '已收到閣下的申請表',
+      html: html
+      });
 
+    
+      // await sails.helpers.sendSingleEmail({
+      //   to: coach.Email,
+      //   from: sails.config.custom.mailgunFrom,
+      //   subject: '已收到閣下的申請表',
+      //   text: 'sdfdsf'
+      // });
 
 
       // return res.redirect('personal_login');
@@ -72,23 +85,73 @@ module.exports = {
   },
 
   confirm_coach: async function (req, res) {
+
+
+    if (!await User.findOne(req.session.userId)) return res.notFound();
+        
+        const thatPerson = await Coach.findOne(req.params.id).populate("worksFor", {id: req.session.userId});
+
+        if (!thatPerson) return res.notFound();
+            
+        if (thatPerson.worksFor.length)
+            return res.status(451).send("Already aprroved.");   // conflict
+  
+     await User.addToCollection(req.session.userId, 'supervises').members([req.params.id]);
+    
+    
+   
     var year = new Date().getFullYear();
     var pid = parseInt(req.params.id) || -1;
+    var modddd = await Coach.findOne(pid);
+    var ConP= modddd.ConfirmPoint;
+    console.log(ConP);
     var num = (year % 100) * 1000;
     var models = await Coach.find({ where: { CoachNo: { '>': num } }, sort: 'CoachNo DESC', limit: 1 });
     var model = models[0];
-    if (models.length > 0) {
+    if (models.length > 0 && ConP==2) {
       var model = await Coach.update(pid).set({
+       
+       comfirm_coach:'是',
         CoachNo: model.CoachNo + 1,
-        comfirm_coach: '是',
+        ConfirmPoint:ConP+1,
+        
+        
       }).fetch();
 
 
-    } else {
+    } 
+   else if (models.length > 0 && ConP<=1) {
+      var model = await Coach.update(pid).set({
+       
+       
+       
+        ConfirmPoint:ConP+1,
+        
+        
+      }).fetch();
+
+
+    } 
+    
+else if(models.length==0&&ConP ==2){
 
       model = await Coach.update(pid).set({
         CoachNo: num + 1,
-        comfirm_coach: '是',
+        ConfirmPoint:ConP+1,
+        comfirm_coach:'是',
+        
+      }).fetch();
+
+
+    }
+
+
+    else if(models.length == 0&&ConP <=1){
+
+      model = await Coach.update(pid).set({
+        
+        ConfirmPoint:ConP+1,
+        
       }).fetch();
 
 
@@ -109,27 +172,66 @@ module.exports = {
     }
   },
 
+  processing_coach: async function (req, res) {
+    // var year = new Date().getFullYear();
+    var pid = parseInt(req.params.id) ||-1;
+  
+    // var num = (year % 100) * 1
+    
+    // var models = await Coach.find({ where: { CoachNo: { '>': num } }, sort: 'CoachNo DESC', limit: 1 });
+    // var model = models[0];
+    // if (models.length > 0) {
+      var model = await Coach.update(pid).set({
+        
+        comfirm_coach: '中',
+       
+       
+      }).fetch();
+
+    // } else {
+      // var model = await Person.findOne(pid);
+
+      // var model = await Coach.create(pid).fetch();
+
+     
+    
+      
+
+
+
+
+      // await Coach.update(model.id).set({
+        
+      //   comfirm_coach: '中',
+      // }).fetch();
+
+
+    // }
+    // model = model[0];
+
+    // var html = await sails.renderView('membership/confirm_Coach', { models: model, layout: false });
+    // await sails.helpers.sendSingleEmail({
+    //   to: '16228375@life.hkbu.edu.hk',
+    //   from: sails.config.custom.mailgunFrom,
+    //   subject: '已確認成為教練',
+    //   html: html
+    // });
+
+
+    if (req.wantsJSON) {
+      return res.json({ message: "Start to review successfully！", url: '/status' });    // for ajax request
+    }
+  },
+
+
+
   cancel_coach: async function (req, res) {
-    var year = new Date().getFullYear();
-    var pid = parseInt(req.params.id) || -1;
-    var num = (year % 100) * 1000;
-    var models = await Coach.find({ where: { CoachNo: { '>': num } }, sort: 'CoachNo DESC', limit: 1 });
-    var model = models[0];
-    if (models.length > 0) {
+   
       var model = await Coach.update(pid).set({
         comfirm_coach: '否',
       }).fetch();
 
 
-    } else {
-
-      model = await Coach.update(pid).set({
-        comfirm_coach: '否',
-      }).fetch();
-
-
-    }
-    model = model[0];
 
     // var html = await sails.renderView('membership/confirm_Coach', { models: model, layout: false });
     // await sails.helpers.sendSingleEmail({
@@ -144,6 +246,11 @@ module.exports = {
       return res.json({ message: "reject successfully！", url: '/status' });    // for ajax request
     }
   },
+
+
+  
+
+ 
 
   update_coach: async function (req, res) {
 
@@ -383,8 +490,25 @@ module.exports = {
   coachform_detail: async function (req, res) {
 
     var coach = await Coach.findOne(req.params.id);
-    if (req.method == 'GET') { return res.view('membership/coachform_detail', { 'coach': coach, 'reg': 0 }); }
+    if (req.method == 'GET') 
+    { return res.view('membership/coachform_detail', 
+    { 'coach': coach, 'reg': 0 }); }
   },
+
+  // coachform_detail: async function (req, res) {
+  //   var coach = await Coach.find(
+  //     {
+      
+  //       Qualification:"選擇",
+  //       Qualification1:"選擇",
+  //       Qualification2:"選擇",
+  //       Qualification3:"選擇",
+
+          
+  //     })
+    
+  //   if (req.method == 'GET') { return res.view('membership/coachform_detail', { 'coach': coach, 'reg': 0 }); }
+  // },
 
 
   canel_coach: async function (req, res) {
@@ -408,25 +532,278 @@ module.exports = {
 
   },
 
-  export_xlsx: async function (req, res) {
-
+  export_xlsx: async function(req, res) {
+    sails.log("enter approved funtion ")
     var models = await Coach.find();
-
     var XLSX = require('xlsx');
     var wb = XLSX.utils.book_new();
 
-    var ws = XLSX.utils.json_to_sheet(models.map(model => {
-      return {
 
-        ChiName: model.ChiName,
-        Id: model.New_coach,
+
+    
+    
+    var ws0 = XLSX.utils.json_to_sheet(models.filter(model=>model.comfirm_coach=="").map(model => {
+      return {
+        教練編號: 'INDC' + model.CoachNo,
+        申請類別: model.New_coach,
+
+        教練註冊級別: model.Level,
+
+        男子競技體操: model.Disciplines1,
+        女子競技體操 : model.Disciplines2,
+        藝術體操: model.Disciplines3,
+        技巧體操: model.Disciplines4,
+        彈網: model.Disciplines5,
+        健美體操: model.Disciplines6,
+        // 個人照片: model.Personal_photo,
+        英文名: model.Engname,
+        中文名: model.ChiName,
+
+        香港身份證號碼: model.Id,
+        性別: model.Gender,
+        出生日期: model.Birthd,
+
+        職業: model.Occupation,
+        學歷: model.Education,
+        住宅: model.Hnumber,
+        手提: model.Mnumber,
+        電郵地址: model.Email,
+        通訊地址: model.ChiAddress,
+        // 狀態: model.Highlight_property5,
+
+        //
+        曾任教本會舉辦之課程: model.HaveBeenCoach,
+        如有: model.Have,
+        其他有關體操教學經驗: model.HaveBeenCoach1,
+        請列明: model.Have2,
+
+        //
+        本會發出之體操教練證書: model.Qualification,
+        證書編號: model.Cert_no,
+        考取年份: model.date_Qualification,
+
+        香港教練培訓委員會發出之證書: model.Qualification1,
+        認可教練號碼: model.Accredited_coachNo,
+        有效日期: model.date_Qualification1,
+        //
+        本會發出之教練理論課程證書: model.Qualification2,
+        證書編號: model.Cert_no2,
+        有效日期: model.date_Qualification2,
+
+
+        急救證書: model.Qualification3,
+        發出機構: model.Issued_by,
+        考取年份: model.date_Qualification3,
+
+
+        其他有關體操教練資歷: model.Qualification4,
+        原因: model.others,
+        是否已考取本地國際裁判資歷: model.judges,
+        考取年份: model.date_qualification5,
+
+        //
+
+        曾於學校或其他地區教授之訓練班舉辦日期: model.date_training,
+        學校機構名稱: model.school_training,
+        教授項目: model.Discipline_training,
+
+        曾教導學生考取章別計劃之數量舉辦日期: model.Badges_date,
+        訓練班學校機構名稱: model.Badges_school,
+        級別及數量: model.Badges_no,
+
+        曾參與總會之體操推廣活動舉辦日期: model.Activities_date,
+        活動名稱: model.Activities_event,
+        負責職位: model.Activities_badges,
+
+        曾參與總會及香港教練培訓委員會之複修課程舉辦日期: model.Coaching_workshops_date,
+        活動名稱: model.Coaching_workshops_event,
+        舉辦機構: model.Coaching_workshops_organization,
+        聲明: model.hope,
+        // 校長簽署: model.avatar_sign,
+
+      }     
+    }));
+
+       var ws1 = XLSX.utils.json_to_sheet(models.filter(model=>model.comfirm_coach=="中").map(model => {
+      return {
+        教練編號: 'INDC' + model.CoachNo,
+        申請類別: model.New_coach,
+
+        教練註冊級別: model.Level,
+
+        男子競技體操: model.Disciplines1,
+        女子競技體操 : model.Disciplines2,
+        藝術體操: model.Disciplines3,
+        技巧體操: model.Disciplines4,
+        彈網: model.Disciplines5,
+        健美體操: model.Disciplines6,
+        // 個人照片: model.Personal_photo,
+        英文名: model.Engname,
+        中文名: model.ChiName,
+
+        香港身份證號碼: model.Id,
+        性別: model.Gender,
+        出生日期: model.Birthd,
+
+        職業: model.Occupation,
+        學歷: model.Education,
+        住宅: model.Hnumber,
+        手提: model.Mnumber,
+        電郵地址: model.Email,
+        通訊地址: model.ChiAddress,
+        // 狀態: model.Highlight_property5,
+
+        //
+        曾任教本會舉辦之課程: model.HaveBeenCoach,
+        如有: model.Have,
+        其他有關體操教學經驗:model.HaveBeenCoach1,
+        請列明: model.Have2,
+
+        //
+        本會發出之體操教練證書: model.Qualification,
+        證書編號: model.Cert_no,
+        考取年份: model.date_Qualification,
+
+        香港教練培訓委員會發出之證書: model.Qualification1,
+        認可教練號碼: model.Accredited_coachNo,
+        有效日期: model.date_Qualification1,
+        //
+        本會發出之教練理論課程證書: model.Qualification2,
+        證書編號: model.Cert_no2,
+        有效日期: model.date_Qualification2,
+
+
+        急救證書: model.Qualification3,
+        發出機構: model.Issued_by,
+        考取年份: model.date_Qualification3,
+
+
+        其他有關體操教練資歷: model.Qualification4,
+        原因: model.others,
+        是否已考取本地國際裁判資歷: model.judges,
+        考取年份: model.date_qualification5,
+
+        //
+
+        曾於學校或其他地區教授之訓練班舉辦日期: model.date_training,
+        學校機構名稱: model.school_training,
+        教授項目: model.Discipline_training,
+
+        曾教導學生考取章別計劃之數量舉辦日期: model.Badges_date,
+        訓練班學校機構名稱: model.Badges_school,
+        級別及數量: model.Badges_no,
+
+        曾參與總會之體操推廣活動舉辦日期: model.Activities_date,
+        活動名稱: model.Activities_event,
+        負責職位: model.Activities_badges,
+
+        曾參與總會及香港教練培訓委員會之複修課程舉辦日期: model.Coaching_workshops_date,
+        活動名稱: model.Coaching_workshops_event,
+        舉辦機構: model.Coaching_workshops_organization,
+        聲明: model.hope,
+        // 校長簽署: model.avatar_sign,
       }
     }));
-    XLSX.utils.book_append_sheet(wb, ws, "Coach");
 
-    res.set("Content-disposition", "attachment; filename=coach.xlsx");
-    return res.end(XLSX.write(wb, { type: "buffer", bookType: "xlsx" }));
 
+
+    var ws2 = XLSX.utils.json_to_sheet(models.filter(model=>model.ConfirmPoint==3).map(model => {
+      return {
+        
+        教練編號: 'INDC' + model.CoachNo,
+        申請類別: model.New_coach,
+
+        教練註冊級別: model.Level,
+
+        男子競技體操: model.Disciplines1,
+        女子競技體操 : model.Disciplines2,
+        藝術體操: model.Disciplines3,
+        技巧體操: model.Disciplines4,
+        彈網: model.Disciplines5,
+        健美體操: model.Disciplines6,
+        // 個人照片: model.Personal_photo,
+        英文名: model.Engname,
+        中文名: model.ChiName,
+
+        香港身份證號碼: model.Id,
+        性別: model.Gender,
+        出生日期: model.Birthd,
+
+        職業: model.Occupation,
+        學歷: model.Education,
+        住宅: model.Hnumber,
+        手提: model.Mnumber,
+        電郵地址: model.Email,
+        通訊地址: model.ChiAddress,
+        // 狀態: model.Highlight_property5,
+
+        //
+        曾任教本會舉辦之課程: model.HaveBeenCoach,
+        如有: model.Have,
+        其他有關體操教學經驗:model.HaveBeenCoach1,
+        請列明: model.Have2,
+
+        //
+        本會發出之體操教練證書: model.Qualification,
+        證書編號: model.Cert_no,
+        考取年份: model.date_Qualification,
+
+        香港教練培訓委員會發出之證書: model.Qualification1,
+        認可教練號碼: model.Accredited_coachNo,
+        有效日期: model.date_Qualification1,
+        //
+        本會發出之教練理論課程證書: model.Qualification2,
+        證書編號: model.Cert_no2,
+        有效日期: model.date_Qualification2,
+
+
+        急救證書: model.Qualification3,
+        發出機構: model.Issued_by,
+        考取年份: model.date_Qualification3,
+
+
+        其他有關體操教練資歷: model.Qualification4,
+        原因: model.others,
+        是否已考取本地國際裁判資歷: model.judges,
+        考取年份: model.date_qualification5,
+
+        //
+
+        曾於學校或其他地區教授之訓練班舉辦日期: model.date_training,
+        學校機構名稱: model.school_training,
+        教授項目: model.Discipline_training,
+
+        曾教導學生考取章別計劃之數量舉辦日期: model.Badges_date,
+        訓練班學校機構名稱: model.Badges_school,
+        級別及數量: model.Badges_no,
+
+        曾參與總會之體操推廣活動舉辦日期: model.Activities_date,
+        活動名稱: model.Activities_event,
+        負責職位: model.Activities_badges,
+
+        曾參與總會及香港教練培訓委員會之複修課程舉辦日期: model.Coaching_workshops_date,
+        活動名稱: model.Coaching_workshops_event,
+        舉辦機構: model.Coaching_workshops_organization,
+        聲明: model.hope,
+        // 校長簽署: model.avatar_sign,
+      }
+    }));
+
+
+
+
+
+
+      XLSX.utils.book_append_sheet(wb, ws0, "待審批");
+      XLSX.utils.book_append_sheet(wb, ws1, "處理中");
+      XLSX.utils.book_append_sheet(wb, ws2, "已審批");
+      
+    
+    res.set("Content-disposition", "attachment; filename=Coach.xlsx");
+    return res.end(XLSX.write(wb, {type:"buffer", bookType:"xlsx"}));
+    
   },
+
+
 
 };
